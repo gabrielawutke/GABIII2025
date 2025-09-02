cf08b8b409b8df30a31b6f8241ab0dd34b267823
gabrielawutke <164521848+gabrielawutke@users.noreply.github.com>
2025-09-02 08:23:08 -0300

    Add files via upload

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,65 +1,78 @@
 export const perguntas = [
     {
         enunciado: "Você sente que tem clareza sobre seus objetivos de vida?",
+
         alternativas: [
             {
                 texto: "Sim, tenho bem claro o que quero alcançar.",
-                afirmacao:[
-                 " Ter clareza sobre o que você quer alcançar é uma base sólida para o seu desenvolvimento. Continue definindo suas metas e ajustando-as conforme necessário.",
-                 "O objetivo de vida não precisa ser único nem definitivo; pode evoluir com quem você é."
-                ]
+
+                afirmacao: [
+                    " Ter clareza sobre o que você quer alcançar é uma base sólida para o seu desenvolvimento. Continue definindo suas metas e ajustando-as conforme necessário.",
+                "O objetivo de vida não precisa ser único nem definitivo; pode evoluir com quem você é."
+
+                ],
+                proxima: 1,
             },
             {
                 texto: " Não, ainda estou buscando clareza.",
-                afirmacao:[
-                 "  Muitas vezes, a clareza vem com a prática e a reflexão. Dedique um tempo para pensar nos seus valores e o que é mais importante para você. Isso pode ajudar a definir um rumo mais claro",
-                 "Encontrar um objetivo de vida costuma exigir autoconhecimento e tempo de reflexão."
-                ]
 
-                
-            }
+                afirmacao: [
+                    "  Muitas vezes, a clareza vem com a prática e a reflexão. Dedique um tempo para pensar nos seus valores e o que é mais importante para você. Isso pode ajudar a definir um rumo mais claro",
+                "Encontrar um objetivo de vida costuma exigir autoconhecimento e tempo de reflexão."
+                ],
+                proxima: 2,
+            },
         ]
     },
     {
         enunciado: "Você tem o hábito de refletir sobre suas emoções e comportamentos?",
+
         alternativas: [
             {
                 texto: "Sim, faço isso regularmente.",
-                afirmacao:[ 
-                 " A autorreflexão é uma ferramenta poderosa para o autoconhecimento. Continue praticando esse hábito, pois ele vai te ajudar a entender melhor suas reações e decisões.",
-                 "Refletir sobre o que traz alegria, propósito e sensação de pertencimento ajuda a delinear esse objetivo."
-                ]
+                afirmacao: [
+                    " A autorreflexão é uma ferramenta poderosa para o autoconhecimento. Continue praticando esse hábito, pois ele vai te ajudar a entender melhor suas reações e decisões.",
+                "Refletir sobre o que traz alegria, propósito e sensação de pertencimento ajuda a delinear esse objetivo."
+                ],
+                proxima: 3,
             },
             {
                 texto: " Não, quase nunca paro para refletir.",
-                afirmacao:[
-                 " A reflexão pode ser algo a ser cultivado com o tempo. Tente reservar alguns momentos do dia para pensar sobre o que sentiu e por que agiu de determinada forma.",
-                 "não precisa ser perfeito ou brilhante desde o começo; pode ser desenvolvido aos poucos."
-                ]
 
-                
-            }
+                afirmacao: [
+                    " A reflexão pode ser algo a ser cultivado com o tempo. Tente reservar alguns momentos do dia para pensar sobre o que sentiu e por que agiu de determinada forma.",
+                "não precisa ser perfeito ou brilhante desde o começo; pode ser desenvolvido aos poucos."
+
+                ],
+                proxima: 4,
+            },
         ]
     },
     {
-        enunciado: "Você acredita que seus desafios atuais contribuem para seu crescimento pessoal?",
+        enunciado:"Você acredita que seus desafios atuais contribuem para seu crescimento pessoal?",
+
         alternativas: [
             {
-                texto: "Sim, vejo os desafios como oportunidades de aprendizado.",
-                afirmacao:[
-                 " Encarar os desafios como oportunidades de crescimento é uma mentalidade muito positiva. Isso pode acelerar sua evolução pessoal e te ajudar a aprender com cada situação.",
-                 "Pequenos passos diários podem sustentar grandes mudanças ao longo do tempo."
-                ]
+                texto:  "Sim, vejo os desafios como oportunidades de aprendizado.",
+
+                afirmacao: [
+                    " Encarar os desafios como oportunidades de crescimento é uma mentalidade muito positiva. Isso pode acelerar sua evolução pessoal e te ajudar a aprender com cada situação.",
+                    "Pequenos passos diários podem sustentar grandes mudanças ao longo do tempo."
+    
+                ],
+                proxima: 3,
             },
             {
                 texto: "Não, sinto que estou apenas enfrentando obstáculos sem muito progresso.",
-                afirmacao:[ 
-                 " É natural se sentir assim em alguns momentos. Tente olhar para seus desafios de outra perspectiva: o que você pode aprender com cada obstáculo? Às vezes, mudar o ângulo de visão faz toda a diferença.",
-                 "entender seus valores facilita encontrar um rumo que faça sentido."
-                ]
 
-            
-            }
+                afirmacao: [
+                    " É natural se sentir assim em alguns momentos. Tente olhar para seus desafios de outra perspectiva: o que você pode aprender com cada obstáculo? Às vezes, mudar o ângulo de visão faz toda a diferença.",
+                    "entender seus valores facilita encontrar um rumo que faça sentido."
+    
+
+                ],
+                proxima: 4,
+            },
         ]
     },
     {
@@ -67,40 +80,53 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Sim, me esforço para me desafiar.",
+
                 afirmacao: [
                     "  Sair da zona de conforto é essencial para o crescimento. Se você já faz isso regularmente, está no caminho certo para evoluir constantemente.",
-                    "não garante sucesso imediato; requer prática e paciência."
-                ]
+                   "não garante sucesso imediato; requer prática e paciência."
 
-                
+                ],
+                proxima: 5,
             },
             {
-                texto: "Não, prefiro agir dentro da minha zona de conforto.",
-                afirmacao: [ 
+                texto:"Não, prefiro agir dentro da minha zona de conforto.",
+
+                afirmacao: [
                     "A zona de conforto oferece segurança, mas também limita o progresso. Comece com pequenos passos fora da sua rotina e observe os resultados.",
                     "Pequenos passos diários podem sustentar grandes mudanças ao longo do tempo."
-                ]
-                
-            }
+ 
+                ],
+                proxima: 6,
+            },
         ]
     },
     {
         enunciado: "Você tem algum tipo de prática regular para cuidar da sua saúde mental?",
+
         alternativas: [
             {
                 texto: "Sim, tenho práticas como meditação, terapia ou atividades relaxantes.",
-                afirmacao:[ 
+                afirmacao: [
                     "Cuidar da sua saúde mental é tão importante quanto cuidar da física. Continue dedicando tempo para o que te ajuda a se manter equilibrado emocionalmente.",
-                    "O sentido da vida não precisa ser único; pode haver várias áreas importantes, como família, carreira, saúde e contribuição social."
-                ]
+                   "O sentido da vida não precisa ser único; pode haver várias áreas importantes, como família, carreira, saúde e contribuição social."
+
+                ],
+                proxima: 5,
             },
             {
+        
                 texto: "Não, não tenho nenhum hábito regular para cuidar disso.",
-                afirmacao:[ 
+
+                afirmacao: [
                     " Muitas pessoas negligenciam a saúde mental. Comece com pequenas práticas, como meditação ou exercícios de respiração, para começar a se sentir mais conectado consigo mesmo.",
                     "não precisa ter tudo definido amanhã; pode evoluir com o tempo."
-                ]
+ 
+                ],
+                
             }
-        ]
+        
+        ],
+
     },
-];
+
+]
